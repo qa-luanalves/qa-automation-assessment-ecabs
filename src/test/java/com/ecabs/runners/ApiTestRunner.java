d925431d4b0aed876edd7fe81861661e3ce68362
Luan Alves <luan_dsa@hotmail.com>
2026-02-25 11:15:48 -0300

    Change name of runner class

--- src/test/java/com/ecabs/runners/ApiTestRunner.java
+++ src/test/java/com/ecabs/runners/ApiTestRunner.java
@@ -1,18 +1,19 @@
 package com.ecabs.runners;
 
+
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "src/test/resources/features/web",
-        glue = "com.ecabs.web",
+        features = "src/test/resources/features/api",
+        glue = "com.ecabs.api",
         plugin = {
                 "pretty",
-                "html:target/cucumber-web-report.html"
+                "html:target/cucumber-api-report.html"
         }
 )
 
-public class WebRunnerTest {
+public class ApiTestRunner {
 }
--- src/test/java/com/ecabs/runners/WebTestRunner.java
+++ src/test/java/com/ecabs/runners/WebTestRunner.java
@@ -1,19 +1,18 @@
 package com.ecabs.runners;
 
-
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "src/test/resources/features/api",
-        glue = "com.ecabs.api",
+        features = "src/test/resources/features/web",
+        glue = "com.ecabs.web",
         plugin = {
                 "pretty",
-                "html:target/cucumber-api-report.html"
+                "html:target/cucumber-web-report.html"
         }
 )
 
-public class ApiRunnerTest {
+public class WebTestRunner {
 }
